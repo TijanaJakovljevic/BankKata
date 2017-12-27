ad4a59f5724b76880e04dd2dd24e1d40ba79298e
NULTIEN\tijana.jakovljevic <tijana.jakovljevic@nultien.rs>
2017-12-27 18:50:06 +0100

    Add console and test projects.

---
+++ BankKata/BankingKata.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankingKata.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BankingKata.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f4b0ef9e-1f46-4881-beed-0bfcf31bb7fd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BankKata/BankingKata.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xunit;
+
+namespace BankingKata.UnitTests
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void TestMethod1()
+		{
+		}
+	}
+}
