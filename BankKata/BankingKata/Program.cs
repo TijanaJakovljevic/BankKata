aa188bc77cc4f2e187982c46b68b94a6b0ee3b0b
NULTIEN\tijana.jakovljevic <tijana.jakovljevic@nultien.rs>
2017-12-27 19:29:43 +0100

    Reformat.

--- BankKata/BankingKata/Program.cs
+++ BankKata/BankingKata/Program.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BankingKata
+﻿namespace BankingKata
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
 		}
 	}
