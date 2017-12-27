ee7c385125bf70874189e4f8c80bafa155615dc2
NULTIEN\tijana.jakovljevic <tijana.jakovljevic@nultien.rs>
2017-12-27 19:28:17 +0100

    Account black box test

---
+++ BankKata/BankingKata.UnitTests/AccountTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BankingKata.UnitTests
+{
+	public class AccountTests
+	{
+		[Fact]
+		public void Should_HaveCorrectBalance_WhenYouDoIntermixedDepositsAndWithdraws()
+		{
+			Account account = new Account();
+
+			account.Deposit(100);
+			account.Deposit(200);
+			account.Withdraw(150);
+			account.Deposit(200);
+			account.Withdraw(50);
+
+			Assert.Equal(300, account.Balance);
+		}
+	}
+}
